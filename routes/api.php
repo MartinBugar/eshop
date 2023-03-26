e7323b7c12a0dde7e630306159398343b6513e2b
martyx <martin1.bugar@gmail.com>
2023-03-26 13:56:20 +0200

    working reading the link without error

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,6 @@
 <?php
 
-use App\Http\Controllers\API\AdminController;
-use App\Http\Controllers\API\ExpensesController;
-use App\Http\Controllers\API\ExpensesTypesController;
-use App\Http\Controllers\API\LoanDetailController;
-use App\Http\Controllers\API\LoansController;
-use App\Http\Controllers\API\PostsController;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -25,13 +19,12 @@ use App\Http\Controllers\API\UserController;
 
 Auth::routes(['verify' => true]);
 
-Route::post('adminlogin', [AdminController::class, 'adminlogin']);
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
 Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::group(['prefix' => 'users', 'middleware' => 'auth:sanctum'], function () {
-    Route::get('/', [AdminController::class, 'index']);
+    Route::get('/', [UserController::class, 'index']);
     Route::post('update/{id}', [UserController::class, 'update']);
     Route::get('edit/{id}', [UserController::class, 'edit']);
     Route::delete('delete/{id}', [UserController::class, 'delete']);
